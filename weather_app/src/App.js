1aee61a8f43dfe9f7b0016d43a8e9f4a4a45eae1
Dhileepguru <dhileepguru497@gmail.com>
2024-03-17 19:28:36 +0530

    Updated code

--- weather_app/src/App.js
+++ weather_app/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    return(
+    <div className=" flex flex-row justify-center h-[100vh] items-center; items-center">
+        <div className="bg-red-500 rounded-md shadow p-2">
+        <h1 className="font-medium">Hey !</h1>
+        <p className="text-xs">Do you want to know the weather report :) </p>
+        <input className=" outline-none rounded-md h-6 mt-2 text-sm p-1" placeholder="City Name"></input>
+        <br/>
+        <button className="bg-black text-white rounded-md p-1 mt-2 text-xs"> Get Report</button>
+        <p className="text-xs mt-2">Degree:123|City:Avinashi|Weather:Rainy </p>
+        </div>
+
+        
+    </div>)
+    
 }
 
-export default App;
+
+
+export default App
